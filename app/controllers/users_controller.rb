ad567a1e8d048ad855f5bdba8e18a81a8752ce4c
Tasha <navancini@gmail.com>
2021-03-11 09:15:29 -0500

    added JSON reading for delete function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,12 +36,15 @@ class UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     @user.destroy
+    
+    render json: @user
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+      render json: @user 
     end
 
     # Only allow a list of trusted parameters through.
